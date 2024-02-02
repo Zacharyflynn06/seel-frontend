1a8a60046c48198e2ea3ac0c3c14b43c75dfa688
Zac Flynn <zacharyflynn06@gmail.com>
2024-02-01 18:05:07 -0700

    add initial nav bar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,13 @@
-<div class="from-pink to-purple fixed inset-0 h-screen w-[20rem] bg-gradient-to-b shadow-2xl"></div>
+<div class="flex h-screen w-full">
+	<div class="from-pink to-purple shadow-04dp h-screen w-[20rem] bg-gradient-to-b"></div>
 
-<div class="bg-off-white h-screen w-full"></div>
+	<div class="bg-off-white flex h-screen w-full items-center justify-center">
+		<div class="shadow-08dp rounded-xl bg-white p-48">
+			<div
+				class="from-purple to-pink bg-gradient-to-r bg-clip-text text-5xl font-extrabold text-transparent"
+			>
+				Hello World
+			</div>
+		</div>
+	</div>
+</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,16 @@ export default {
 				purple: '#4b4a78',
 				'light-purple': '#9f9efa',
 				pink: '#d0577b'
+			},
+			boxShadow: {
+				DEFAULT: '0 1px 2px 0 rgba(0, 0, 0, 0.05), 0 4px 8px 0 rgba(0, 0, 0, 0.05)',
+				'01dp': '0px 2.5px 2.5px -1.5px rgba(0, 0, 0, 0.12)',
+				'04dp': '0px 6px 12px -6px rgba(0, 0, 0, 0.16)',
+				'08dp': '0px 6px 12px -6px rgba(0, 0, 0, 0.2), 0px 8px 24px -4px rgba(0, 0, 0, 0.08)',
+				'24dp': '0px 8px 16px 2px rgba(0, 0, 0, 0.1), 0px 4px 4px -2px rgba(0, 0, 0, 0.2)',
+				red: '0px 0px 4px 1px rgba(246, 102, 111, 0.25)',
+				gold: '0px 0px 4px 1px rgba(255, 122, 0, 0.45)',
+				autofill: '0 0 0 30px var(--tw-shadow-color) inset !important '
 			}
 		}
 	},
