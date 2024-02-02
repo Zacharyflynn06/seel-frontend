20ce98d893377e3f03fd4a95333ea3fa52a4fa28
Zac Flynn <zacharyflynn06@gmail.com>
2024-02-01 17:26:34 -0700

    add colors from deck and add gradient nav bar to landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,3 @@
-<h1 class="flex text-3xl font-bold text-blue-700 underline">Hello world!!!</h1>
+<div class="from-pink to-purple fixed inset-0 h-screen w-[20rem] bg-gradient-to-b shadow-2xl"></div>
+
+<div class="bg-off-white h-screen w-full"></div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				'off-white': '#f2f2f2',
+				'off-black': '#333333',
+				purple: '#4b4a78',
+				'light-purple': '#9f9efa',
+				pink: '#d0577b'
+			}
+		}
 	},
 	plugins: []
 };
