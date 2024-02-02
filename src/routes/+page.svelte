1a9395cdfa4120b0d1382b62c67c3f044d23d678
Zac Flynn <zacharyflynn06@gmail.com>
2024-02-01 18:10:05 -0700

    add quick hover and active states

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,9 @@
 	<div class="from-pink to-purple shadow-04dp h-screen w-[20rem] bg-gradient-to-b"></div>
 
 	<div class="bg-off-white flex h-screen w-full items-center justify-center">
-		<div class="shadow-08dp rounded-xl bg-white p-48">
+		<div
+			class="shadow-08dp hover:shadow-24dp active:shadow-01dp transform rounded-xl bg-white p-48 transition-all duration-500 ease-in-out hover:scale-[101%] active:scale-[99%]"
+		>
 			<div
 				class="from-purple to-pink bg-gradient-to-r bg-clip-text text-5xl font-extrabold text-transparent"
 			>
