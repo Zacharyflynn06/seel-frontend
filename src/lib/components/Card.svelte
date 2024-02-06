b6ceb452cf0044afd40ba5beeed17a7fae5f2fa1
Zac Flynn <zacharyflynn06@gmail.com>
2024-02-06 14:51:45 -0700

    add app layout, nav menu, icons, logos and initial route layout

---
+++ src/lib/components/Card.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	export let data: { heading: string; body: string };
+</script>
+
+<div
+	class="shadow-08dp hover:shadow-24dp active:shadow-01dp transform rounded-2xl bg-white transition-all duration-500 ease-in-out hover:scale-[101%] active:scale-[99%]"
+>
+	<div>
+		<h2 class="p-5 text-2xl font-bold">{data.heading}</h2>
+		<p class="p-5 text-lg">{data.body}</p>
+	</div>
+</div>
