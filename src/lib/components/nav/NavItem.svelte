5ace11b2c93f0e209c5ad36e3692cb7e916df8c7
Zac Flynn <zacharyflynn06@gmail.com>
2024-02-13 23:05:29 -0700

    update icons, install tailwind forms, add semantic html tags

--- src/lib/components/nav/NavItem.svelte
+++ src/lib/components/nav/NavItem.svelte
@@ -6,7 +6,7 @@
 
 <a
 	href={navObj.href}
-	class="flex flex-col items-center space-y-2.5 transition-all duration-500 ease-in-out hover:scale-[102.5%] active:scale-[97.5%]"
+	class="flex transform-gpu flex-col items-center space-y-2.5 transition-all duration-500 ease-in-out hover:scale-[102.5%] active:scale-[97.5%]"
 >
 	<div
 		class="h-12 w-12 rounded-xl bg-white p-3 text-light-purple shadow-08dp transition-all duration-500 ease-in-out hover:shadow-24dp active:shadow-04dp md:h-14 md:w-14 md:rounded-2xl dark:bg-off-black"
