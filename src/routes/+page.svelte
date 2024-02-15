4da93ec23aeb39e0faa5db6e7ba5aee01e60d32f
Zac Flynn <zacharyflynn06@gmail.com>
2024-02-15 15:08:47 -0700

    add submit button inside input, add Spinner and loading state

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,54 @@
+<script>
+	import { animatedTouchClasses } from '$lib/classes';
+	import Spinner from '$lib/components/Spinner.svelte';
+	import ArrowIcon from '$lib/components/icons/ArrowIcon.svelte';
+	let loading = false;
+
+	function handleSubmit() {
+		loading = !loading;
+	}
+</script>
+
 <main
 	class="gradient-animation grid h-[100dvh] w-full items-center justify-center overflow-hidden bg-gradient-to-bl from-pink to-purple"
 >
-	<form class="flex max-w-[500px] flex-col items-center justify-between">
-		<img src="/images/SeelLogo.png" class="h-32 drop-shadow-2xl" />
+	<form
+		on:submit={handleSubmit}
+		class="flex max-w-[500px] flex-col items-center justify-between space-y-5"
+	>
+		<div>
+			<img
+				alt="Seel logo"
+				src="/images/SeelLogo.png"
+				class="spinner-animate h-32 drop-shadow-2xl"
+			/>
 
-		<h1
-			class="font-spartan text-7xl font-extrabold text-off-white drop-shadow-2xl dark:text-off-black"
+			<h1
+				class="font-spartan text-7xl font-extrabold text-off-white drop-shadow-2xl dark:text-off-black"
+			>
+				Seel
+			</h1>
+		</div>
+		<div
+			class="focus: relative grid w-full items-center rounded-lg bg-off-white text-center drop-shadow-2xl"
 		>
-			Seel
-		</h1>
-		<div class="grid w-full items-center justify-center space-y-5 text-center">
+			<button
+				type="submit"
+				class="absolute right-1.5 z-10 h-full transform bg-transparent pl-1 text-off-black ease-in-out {animatedTouchClasses}"
+			>
+				{#if !loading}
+					<ArrowIcon className="h-6 w-6" />
+				{:else}
+					<Spinner />
+				{/if}
+			</button>
 			<input
 				placeholder="Ask me about Seel..."
 				type="text"
-				class="border-1 rounded-lg bg-off-white px-3 py-1.5 ring-0 drop-shadow-2xl focus-within:ring-0 focus:border-light-purple focus:ring-0"
+				class="focus-ring-0 mr-8 rounded-l-lg border-transparent bg-transparent px-3 py-1.5 ring-0"
 			/>
-			<div class="text-off-white drop-shadow-2xl">this is where the response will animate in</div>
 		</div>
+		<div class="text-off-white drop-shadow-2xl">this is where the response will animate in</div>
 	</form>
 </main>
 
