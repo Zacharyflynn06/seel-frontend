c38ffc8127567d8befbbd64a0a06f6a348be26e8
Zac Flynn <zacharyflynn06@gmail.com>
2024-03-07 19:57:56 -0700

    WIP dashboard components

--- src/lib/classes.ts
+++ src/lib/classes.ts
@@ -5,6 +5,8 @@ export const mainPanelWidth = 'w-full md:w-[calc(100vw-10rem)]';
 export const animatedTouchClasses =
 	'duration-400 transform-gpu transition-all ease-in-out hover:scale-101 active:scale-99 shadow-04dp hover:shadow-08dp active:shadow-01dp hover:brightness-105 active:brightness-95';
 
+export const standardHoverClasses = 'hover:bg-black/25 dark:hover:bg-white/25';
+
 export const zeroDp = 'shadow-01dp dark:shadow-01dp bg-off-white dark:bg-black';
 
 // input classes
@@ -14,7 +16,8 @@ export const inputBaseColors = 'bg-transparent';
 
 export const selectBoxClasses = `py-1 rounded-md border border-black/50 dark:border-white/50 dark:focus:border-light-purple focus:border-light-purple capitalize shadow-04dp  ${inputBaseColors} ${inputResetClasses} `;
 
-export const cardHeadingClasses = 'font-spartan text-2xl font-bold text-black dark:text-white';
+export const cardHeadingClasses =
+	'font-spartan text-xl font-bold text-black dark:text-white leading-[100%]';
 
 // buttons
 export const smallButtonClasses =
--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -7,7 +7,15 @@
 	export let headingIsCentered = false;
 </script>
 
-<section class="space-y-2 rounded-2xl bg-white p-5 shadow-08dp dark:bg-off-black {className}">
-	<h2 class="{cardHeadingClasses} {headingIsCentered ? 'text-center' : ''}">{heading}</h2>
-	<slot />
+<section
+	class="w-full space-y-2 rounded-2xl bg-white p-5 shadow-08dp dark:bg-off-black {className}"
+>
+	{#if heading}
+		<h2 class="{cardHeadingClasses} {headingIsCentered ? 'text-center' : ''} h-[10%]">
+			{heading}
+		</h2>
+	{/if}
+	<div class={heading ? 'h-[90%]' : 'h-full'}>
+		<slot />
+	</div>
 </section>
