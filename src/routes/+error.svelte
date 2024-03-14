951340017533b5dc6f92695915996c6c215960fd
Zac Flynn <zacharyflynn06@gmail.com>
2024-03-14 13:07:34 -0600

    more updates to error.svelte

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -5,7 +5,7 @@
 <div class="grid h-screen w-full items-center justify-center bg-gradient-to-bl from-pink to-purple">
 	<div class="flex flex-col items-center justify-center">
 		<img class="h-[10rem] w-[10rem] drop-shadow-2xl" src="images/SeelLogo.png" alt="" />
-		<div class="text-center font-spartan font-thin text-white">
+		<div class="font text-center font-spartan text-2xl text-white">
 			<h1 class="text-7xl">We're Sorry!</h1>
 			<h1>Something Went Wrong</h1>
 
@@ -13,9 +13,3 @@
 		</div>
 	</div>
 </div>
-
-<style>
-	h1 {
-		@apply text-2xl font-extrabold;
-	}
-</style>
