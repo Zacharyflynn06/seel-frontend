3495bfdf60de96586bcd869acab6474514be6a37
Zac Flynn <zacharyflynn06@gmail.com>
2024-03-14 12:59:27 -0600

    update error.svelte

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -4,9 +4,9 @@
 
 <div class="grid h-screen w-full items-center justify-center bg-gradient-to-bl from-pink to-purple">
 	<div class="flex flex-col items-center justify-center">
-		<img class="drop-shadow-2xl" src="images/SeelLogo.png" alt="" />
-		<div class="text-center text-white">
-			<h1>We're Sorry!</h1>
+		<img class="h-[10rem] w-[10rem] drop-shadow-2xl" src="images/SeelLogo.png" alt="" />
+		<div class="text-center font-spartan font-thin text-white">
+			<h1 class="text-7xl">We're Sorry!</h1>
 			<h1>Something Went Wrong</h1>
 
 			<p>{$page.status} - {$page.error?.message}</p>
