1a401a8eb172c357847ad671d47b5f8db7531c29
Zac Flynn <zacharyflynn06@gmail.com>
2024-03-29 19:30:34 -0600

    remove unused page.server file

--- $houdini/types/src/routes/(auth)/$houdini.d.ts
+++ $houdini/types/src/routes/(auth)/$houdini.d.ts
@@ -11,7 +11,7 @@ type OutputDataShape<T> = MaybeWithVoid<Omit<App.PageData, RequiredKeys<T>> & Pa
 type EnsureDefined<T> = T extends null | undefined ? {} : T;
 type OptionalUnion<U extends Record<string, any>, A extends keyof U = U extends U ? keyof U : never> = U extends unknown ? { [P in Exclude<A, keyof U>]?: never } & U : never;
 export type Snapshot<T = any> = Kit.Snapshot<T>;
-type LayoutRouteId = RouteId | "/(auth)/forgot-password" | "/(auth)/log-in" | "/(auth)/new-password" | "/(auth)/sign-up" | "/(auth)/signin"
+type LayoutRouteId = RouteId | "/(auth)/forgot-password" | "/(auth)/log-in" | "/(auth)/new-password" | "/(auth)/sign-up"
 type LayoutParams = RouteParams & {  }
 type LayoutParentData = EnsureDefined<import('../$houdini').LayoutData>;
 						type MakeOptional<Target, Keys extends keyof Target> = Omit<Target, Keys> & {
