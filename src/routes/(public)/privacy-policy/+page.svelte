984ea3ba8abacecc7cb1a2b890f508be5b176671
Zac Flynn <zacharyflynn06@gmail.com>
2024-06-10 14:27:28 -0600

    add privacy policy

---
+++ src/routes/(public)/privacy-policy/+page.svelte
@@ -0,0 +1,151 @@
+<script lang="ts">
+	// your script goes here
+</script>
+
+<div class="container mx-auto p-5">
+	<h1 class="mb-5 text-4xl font-bold">Privacy Policy</h1>
+
+	<section class="mb-5">
+		<h2 class="mb-2.5 text-2xl font-semibold">1. Introduction</h2>
+		<p class="mb-5">
+			Seel is committed to protecting your privacy. This Privacy Policy explains how we collect,
+			use, disclose, and safeguard your information when you visit our website <a
+				href="seeldeals.com"
+				class="text-blue-600 underline">seeldeals.com</a
+			>, including any other media form, media channel, mobile website, or mobile application
+			related or connected thereto (collectively, the "Site"). Please read this privacy policy
+			carefully. If you do not agree with the terms of this privacy policy, please do not access the
+			site.
+		</p>
+	</section>
+
+	<section class="mb-5">
+		<h2 class="mb-2.5 text-2xl font-semibold">2. Information We Collect</h2>
+		<p class="mb-5">
+			We may collect information about you in a variety of ways. The information we may collect on
+			the Site includes:
+		</p>
+
+		<h3 class="mb-2.5 text-xl font-semibold">Personal Data</h3>
+		<p class="mb-5">
+			Personally identifiable information, such as your name, shipping address, email address, and
+			telephone number, and demographic information, such as your age, gender, hometown, and
+			interests, that you voluntarily give to us when you register with the Site or when you choose
+			to participate in various activities related to the Site, such as online chat and message
+			boards.
+		</p>
+
+		<h3 class="mb-2.5 text-xl font-semibold">Derivative Data</h3>
+		<p class="mb-5">
+			Information our servers automatically collect when you access the Site, such as your IP
+			address, your browser type, your operating system, your access times, and the pages you have
+			viewed directly before and after accessing the Site.
+		</p>
+
+		<h3 class="mb-2.5 text-xl font-semibold">Financial Data</h3>
+		<p class="mb-5">
+			Financial information, such as data related to your payment method (e.g., valid credit card
+			number, card brand, expiration date) that we may collect when you purchase, order, return,
+			exchange, or request information about our services from the Site. [We store only very
+			limited, if any, financial information that we collect.]
+		</p>
+
+		<h3 class="mb-2.5 text-xl font-semibold">Mobile Device Data</h3>
+		<p class="mb-5">
+			Device information, such as your mobile device ID, model, and manufacturer, and information
+			about the location of your device, if you access the Site from a mobile device.
+		</p>
+	</section>
+
+	<section class="mb-5">
+		<h2 class="mb-2.5 text-2xl font-semibold">3. Use of Your Information</h2>
+		<p class="mb-5">
+			Having accurate information about you permits us to provide you with a smooth, efficient, and
+			customized experience. Specifically, we may use information collected about you via the Site
+			to:
+		</p>
+		<ul class="mb-5 list-inside list-disc">
+			<li>Create and manage your account.</li>
+			<li>Process your transactions and send you related information.</li>
+			<li>Improve our website to better serve you.</li>
+			<li>Understand and analyze how you use our website.</li>
+			<li>Send you promotional materials.</li>
+			<li>Respond to your customer service requests and support needs.</li>
+			<li>Fulfill and manage purchases, orders, payments, and other transactions.</li>
+			<li>
+				Communicate with you about your account or transactions with us and send you information
+				about our features and policies.
+			</li>
+		</ul>
+	</section>
+
+	<section class="mb-5">
+		<h2 class="mb-2.5 text-2xl font-semibold">4. Disclosure of Your Information</h2>
+		<p class="mb-5">
+			We may share information we have collected about you in certain situations. Your information
+			may be disclosed as follows:
+		</p>
+
+		<h3 class="mb-2.5 text-xl font-semibold">By Law or to Protect Rights</h3>
+		<p class="mb-5">
+			If we believe the release of information about you is necessary to respond to legal process,
+			to investigate or remedy potential violations of our policies, or to protect the rights,
+			property, and safety of others, we may share your information as permitted or required by any
+			applicable law, rule, or regulation.
+		</p>
+
+		<h3 class="mb-2.5 text-xl font-semibold">Third-Party Service Providers</h3>
+		<p class="mb-5">
+			We may share your information with third parties that perform services for us or on our
+			behalf, including payment processing, data analysis, email delivery, hosting services,
+			customer service, and marketing assistance.
+		</p>
+
+		<h3 class="mb-2.5 text-xl font-semibold">Business Transfers</h3>
+		<p class="mb-5">
+			We may share or transfer your information in connection with, or during negotiations of, any
+			merger, sale of company assets, financing, or acquisition of all or a portion of our business
+			to another company.
+		</p>
+	</section>
+
+	<section class="mb-5">
+		<h2 class="mb-2.5 text-2xl font-semibold">5. Security of Your Information</h2>
+		<p class="mb-5">
+			We use administrative, technical, and physical security measures to help protect your personal
+			information. While we have taken reasonable steps to secure the personal information you
+			provide to us, please be aware that despite our efforts, no security measures are perfect or
+			impenetrable, and no method of data transmission can be guaranteed against any interception or
+			other type of misuse.
+		</p>
+	</section>
+
+	<section class="mb-5">
+		<h2 class="mb-2.5 text-2xl font-semibold">6. Policy for Children</h2>
+		<p class="mb-5">
+			We do not knowingly solicit information from or market to children under the age of 13. If we
+			learn we have collected personal information from a child under age 13 without verification of
+			parental consent, we will delete that information as quickly as possible.
+		</p>
+	</section>
+
+	<section class="mb-5">
+		<h2 class="mb-2.5 text-2xl font-semibold">7. Changes to This Privacy Policy</h2>
+		<p class="mb-5">
+			We may update this Privacy Policy from time to time in order to reflect, for example, changes
+			to our practices or for other operational, legal, or regulatory reasons. We will notify you of
+			any changes by posting the new Privacy Policy on the Site. You are advised to review this
+			Privacy Policy periodically for any changes.
+		</p>
+	</section>
+
+	<section class="mb-5">
+		<h2 class="mb-2.5 text-2xl font-semibold">8. Contact Us</h2>
+		<p class="mb-5">
+			If you have questions or comments about this Privacy Policy, please contact us at:
+		</p>
+		<p class="text-lg font-semibold">
+			<a href="mailto:support@seeldeals.com">support@seeldeals.com</a>
+		</p>
+	</section>
+</div>
