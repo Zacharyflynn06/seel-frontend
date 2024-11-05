b255ce5e9e393c3a98ab1e4c2fec92dc752ab161
Zac Flynn <zacharyflynn06@gmail.com>
2024-11-04 18:13:42 -0700

    fix dark mode issues on job description

--- src/routes/(public)/careers/product-designer/+page.svelte
+++ src/routes/(public)/careers/product-designer/+page.svelte
@@ -3,9 +3,11 @@
 </script>
 
 <Card width="md:max-w-[1100px] " bgColor="bg-white">
-	<div class="flex flex-col items-center justify-center space-y-5 px-10 py-20 text-center">
+	<div
+		class="flex w-full flex-col items-center justify-center space-y-5 py-20 text-center md:px-10"
+	>
 		<div class="flex flex-col items-center uppercase">
-			<h1 class="text-7xl text-purple">Product Designer</h1>
+			<h1 class="text-7xl text-purple dark:text-white">Product Designer</h1>
 			<h2 class="text-3xl text-pink">part time | remote</h2>
 		</div>
 
@@ -13,7 +15,7 @@
 			Job Description
 		</div>
 
-		<p class="text-lg font-bold text-purple">
+		<p class="text-lg font-bold text-purple dark:text-white">
 			As a Product Designer at Seel, you will play a crucial role in shaping the user experience of
 			our platform. You will collaborate with developers to design and iterate on features, ensuring
 			that they are not only functional but also aligned with the needs of our users—private equity
@@ -24,7 +26,7 @@
 
 		<div class="text-xl font-bold text-pink">ABOUT SEEL</div>
 
-		<p class="text-purple">
+		<p class="text-purple dark:text-white">
 			Seel is an innovative SaaS platform designed to simplify and enhance private equity and
 			venture capital deal execution by automating key tasks like qualifying deals, conducting due
 			diligence, and writing investment memos. We’re seeking a Product Designer to bring the vision
@@ -36,8 +38,8 @@
 		<div
 			class="h-fit w-full rounded-3xl bg-gradient-to-b from-pink to-purple text-left text-white md:flex md:gap-5"
 		>
-			<div class="px-5 py-10 md:w-2/3 md:pl-10">
-				<h1 class="text-3xl font-bold">KEY RESPONSIBILITIES:</h1>
+			<div class="space-y-5 px-5 py-10 md:w-2/3 md:pl-10">
+				<h1 class="text-2xlmd:text-3xl font-bold">KEY RESPONSIBILITIES:</h1>
 				<ul class="ml-5 list-disc space-y-2.5">
 					<li>
 						<span class="font-bold">Design and Prototyping:</span> Create wireframes, mockups, and high-fidelity
@@ -75,8 +77,8 @@
 					</li>
 				</ul>
 			</div>
-			<div class="px-5 py-10 md:w-1/3 md:pr-10">
-				<h1 class="text-3xl font-bold">REQUIREMENTS:</h1>
+			<div class="space-y-5 px-5 py-10 md:w-1/3 md:pr-10">
+				<h1 class="text-2xl font-bold md:text-3xl">REQUIREMENTS:</h1>
 				<ul class="ml-5 list-disc space-y-2.5">
 					<li>Proven experience as a Product Designer, preferably in a SaaS or tech company.</li>
 					<li>Strong portfolio showcasing your design process and problem- solving skills.</li>
