2ea288da972fc8de1f62a5823a2b895c4f29531f
Zac Flynn <zacharyflynn06@gmail.com>
2024-11-07 13:08:45 -0700

    uninstall houdini, rip out folders autogenerated by houdini

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
-import { nodeLoaderPlugin } from '@vavite/node-loader/plugin';
 
 /** @type {import('vite').UserConfig} */
-export default defineConfig(({ mode }) => {
-	let plugins = [sveltekit()];
-	if (mode === 'development') {
-		plugins = [nodeLoaderPlugin(), sveltekit()];
-	}
+export default defineConfig(() => {
+	const plugins = [sveltekit()];
 
 	return {
 		plugins
