b8e5612ca1a241f833d1862e7307f4bd422de109
Zac Flynn <zacharyflynn06@gmail.com>
2024-11-21 11:22:37 -0700

    add comments in the fetch wrapper

--- src/lib/services/fetchWrapper.ts
+++ src/lib/services/fetchWrapper.ts
@@ -1,3 +1,4 @@
+// Function to handle error responses by returning a Response object with error details
 const handleErrorResponse = (error: string, status = 400) => {
 	return new Response(
 		JSON.stringify({
@@ -8,17 +9,22 @@ const handleErrorResponse = (error: string, status = 400) => {
 	);
 };
 
+// Define supported HTTP methods
 type HttpMethod = 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH';
+
+// Perform a fetch request with the provided parameters
 export const performFetch = async (
 	path: string,
 	method: HttpMethod,
 	body: Record<string, unknown> | null,
 	token: string | null
 ) => {
+	// Set up headers for the request
 	let headers: { 'Content-Type': string; Authorization?: string } = {
 		'Content-Type': 'application/json'
 	};
 
+	// Add authorization header if token is provided
 	if (token) {
 		headers = {
 			...headers,
@@ -26,18 +32,22 @@ export const performFetch = async (
 		};
 	}
 
+	// Initialize fetch options
 	const init: RequestInit = {
 		method,
 		headers,
 		body: body ? JSON.stringify(body) : null
 	};
 
+	// Perform the fetch request
 	const response = await fetch(`${import.meta.env.VITE_API_URL}${path}`, init);
 
+	// Handle error responses
 	if (response.status >= 400) {
 		const resBody = await response.json();
 		return handleErrorResponse(resBody.error, response.status);
 	}
 
+	// Return the response body as JSON
 	return response.json();
 };
