97c916f194c88ce3d221f6e782dc518519b8cc8c
Zac Flynn <zacharyflynn06@gmail.com>
2024-11-25 11:02:21 -0600

    pull out houdini from config

--- src/routes/(public)/+page.server.ts
+++ src/routes/(public)/+page.server.ts
@@ -1,4 +1,4 @@
-import type { PageServerLoad } from './$houdini';
+import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async (event) => {
 	console.log(event.locals);
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
-import houdini from 'houdini/vite';
 import { nodeLoaderPlugin } from '@vavite/node-loader/plugin';
 
 /** @type {import('vite').UserConfig} */
 export default defineConfig(({ mode }) => {
-	let plugins = [houdini(), sveltekit()];
+	let plugins = [sveltekit()];
 	if (mode === 'development') {
-		plugins = [houdini(), nodeLoaderPlugin(), sveltekit()];
+		plugins = [nodeLoaderPlugin(), sveltekit()];
 	}
 
 	return {
